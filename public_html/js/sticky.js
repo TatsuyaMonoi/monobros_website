abd7dad1be9812f2c560e5b38ffc3754708fb58f
物井達也 <t.monoi117@gmail.com>
2015-10-29 01:02:18 +0900

    修正

--- public_html/js/sticky.js
+++ public_html/js/sticky.js
@@ -14,13 +14,13 @@ topContent = $content.offset().top; //#contentの位置を取得
 var sticky = false;
  
  $window.on("scroll", function () {
-      if ($window.scrollTop() > topContent) {　//scroll位置が#contentの上にある場合
+      if ($window.scrollTop() > topContent) {//scroll位置が#contentの上にある場合
            if ( sticky === false ){
-                $chenge.slideDown();　//#change部分が上がる。
+                $chenge.slideDown();//#change部分が上がる。
                 sticky = true;
            }
       } else {
-           if ( sticky === true ){　//scroll位置が下にある場合
+           if ( sticky === true ){//scroll位置が下にある場合
                 $chenge.slideUp();//#change部分が降りてくる。
                 sticky = false;
            }
